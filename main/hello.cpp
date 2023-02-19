a2aa13f639e546524477e984af9abc69650bbcb7
Vaisnavi382 <71135394+Vaisnavi382@users.noreply.github.com>
2023-02-19 13:19:09 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I am PES2UG20CS379\n";
+  cout << "I am PES2UG20CS379 from 6F\n";
   return 0;
 }
